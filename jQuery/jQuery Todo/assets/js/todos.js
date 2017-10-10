099026a01c43a1630397f42c466f03ebe8e245b9
cyncx <30388906+cyncx@users.noreply.github.com>
2017-10-10 21:25:55 +0100

    made a todo list!

--- jQuery/jQuery Todo/assets/js/todos.js
+++ jQuery/jQuery Todo/assets/js/todos.js
@@ -1,7 +1,7 @@
 //strike through completed todos
-$('li').click(function(){
+$("ul").on("click", "li", function(){
 	//Do this!
-	$(this).toggleClass('disabled');
+	$(this).toggleClass("disabled");
 
 	//Not this!!
 	/*if ($(this).css('color') === "rgb(128, 128, 128)") {
@@ -17,10 +17,17 @@ $('li').click(function(){
 		})
 	}*/
 });
-$('li span').click(function(e){
+$("ul").on("click", ".bin", function(e){
 	var item = $(this).parent()
 	item.fadeOut(500, function(){
 		item.remove();
 	});
 	e.stopPropagation();
+});
+$("input[type='text']").keypress(function(e){
+	if (e.which === 13) {		
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span class='check'></span>" + todoText + " <span class='bin'><i class='fa fa-trash-o'></i></span></li>");
+	}
 });
